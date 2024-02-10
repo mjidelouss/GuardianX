ce94f67a8245907865c26c19d688058da5229175
mjidelouss <mjid.elouss@gmail.com>
2024-02-10 15:55:00 +0100

    Added Home Component

--- GuardianX-FrontEnd/src/app/home/home.component.ts
+++ GuardianX-FrontEnd/src/app/home/home.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UserService} from "../_services/user.service";
+import {AuthService} from "../_services/auth.service";
+import {UserModel} from "../_models/user.model";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  pubContent : string = '';
+  user! : UserModel;
+  AuthUserSub! : Subscription;
+  constructor(
+    private userService : UserService,
+    private authService : AuthService
+  ) {
+  }
+  ngOnInit(): void {
+
+    this.AuthUserSub = this.authService.AuthenticatedUser$.subscribe({
+      next : user => {
+        if(user) this.user = user;
+      }
+    })
+
+    this.userService.getUserPublicContent().subscribe({
+      next : data => {
+        this.pubContent = data;
+      },
+      error : err => console.log(err)
+    })
+  }
+  ngOnDestroy() {
+    this.AuthUserSub.unsubscribe();
+  }
 }
