c41e9ce744cee8a1e8d9d0bfcbc35793080ef7f6
mjidelouss <mjid.elouss@gmail.com>
2024-02-10 15:37:57 +0100

    Added Auth Service and Storage Service

--- GuardianX-FrontEnd/src/app/_services/storage.service.ts
+++ GuardianX-FrontEnd/src/app/_services/storage.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { UserModel } from '../_models/user.model';
+
+const USER_KEY = 'authenticated-user';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,20 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   constructor() { }
+  
+  saveUser(user : UserModel){
+    window.localStorage.removeItem(USER_KEY);
+    window.localStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+  getSavedUser() : UserModel | null {
+    const user = window.localStorage.getItem(USER_KEY);
+    if (user) {
+      return JSON.parse(user);
+    }
+    return null;
+  }
+
+  clean(): void {
+    window.localStorage.clear();
+  }
 }
